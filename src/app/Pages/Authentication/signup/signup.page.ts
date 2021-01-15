85307b9b23e519da844599245880ece3bf3b17ff
Oluwaseun Odeyemi <P7958@fidelitybank.ng>
2021-01-15 09:52:37 +0100

    login and signup completed

--- src/app/Pages/Authentication/signup/signup.page.ts
+++ src/app/Pages/Authentication/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Handlers } from 'src/Business/Constants/handlers';
+import { DisplayServices } from 'src/Business/Display/displayServices';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  pressedOnce;
+
+  constructor(
+    public handlers: Handlers,
+    public displayServices: DisplayServices,
+    public router: Router
+  ) {
+    this.pressedOnce = false;
+  }
+
+  back() {
+    this.pressedOnce = true;
+    this.handlers.navBack()
+  }
+
+  login() {
+    this.router.navigate(['dashboard'])
+  }
 
   ngOnInit() {
   }
