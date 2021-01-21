c871942e89105909283e15e07c410a5324cd4e23
Oluwaseun Odeyemi <P7958@fidelitybank.ng>
2021-01-21 20:04:41 +0100

    progress

--- src/app/Pages/Dashboard/dashboard/buy-course/payment/payment.page.ts
+++ src/app/Pages/Dashboard/dashboard/buy-course/payment/payment.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Handlers } from 'src/Business/Constants/handlers';
+import { DisplayServices } from 'src/Business/Display/displayServices';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentPage implements OnInit {
 
-  constructor() { }
+  pressedOnce;
+  doSaveCard
+
+  constructor(
+    public handlers: Handlers,
+    public displayServices: DisplayServices,
+    public router: Router
+  ) {
+    this.pressedOnce = false;
+  }
+
+  back() {
+    this.pressedOnce = true;
+    this.handlers.navBack()
+  }
+
+  login() {
+    this.router.navigate(['dashboard'])
+  }
 
   ngOnInit() {
+  }
+
+  saveCard() {
+
   }
 
 }
